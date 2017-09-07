a914a41c69270b529b4aa6171223b9002772a0be
Ing. Stefan Polzer <stefan@posit.at>
2017-09-07 13:37:59 +0200

    Add script to add and config php-fpm users

---
+++ php/add-fpm-user.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+username=$1;
+
+if [ $username = "" ] ; then
+	echo "Wrong syntay: add-fpm-user [username]";
+fi
+
+(echo "$username" | grep -Eq  "^[a-z][a-z0-9]{2,14}$") || (echo "User did not match credentials. Only a-z and numbers, start with a character and a total length between 3 and 15."; exit;);
+
+## get php Version
+php_version="$(php -r '$v = phpversion(); echo substr($v, 0,3);')";
+case $php_version in
+	7.[0-9] ) break;;
+	* ) echo "You need PHP version 7+"; exit;;
+esac
+
+echo $username;
+echo $php_version;
+
+touch /etc/php/$php_version/fpm/pool.d/$username.conf;
